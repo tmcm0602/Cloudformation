36ae74befd205a8be0aa200226406b929540cd5c
tmcm0602 <thomas.mcmahon@student.uts.edu.au>
2025-04-05 11:45:44 +1100

    Test changes
    test pipeline

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
-  };
-  return response;
-};
+    body: JSON.stringify("What are you doing!"),
+  }
+  return response
+}
