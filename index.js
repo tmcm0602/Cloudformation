869f8cf552d49135175d58c31c9b05f94d5c2785
tmcm0602 <thomas.mcmahon@student.uts.edu.au>
2025-04-06 10:46:41 +1000

    pipeline test
    pipeline test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export const handler = async (event) => {
   // TODO implement
   const response = {
     statusCode: 200,
-    body: JSON.stringify("What are you doing!"),
+    body: JSON.stringify("Hi What are u even!"),
   }
   return response
 }
